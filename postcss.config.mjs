410124ba21f15c6c22c481615a1dc6218c74dcbf
Isaac Kelly <isaacwwkelly@gmail.com>
2025-02-17 11:10:44 -0600

    vercel deployment things

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  content: [
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-  ],
   plugins: {
     "@tailwindcss/postcss": {},
   },
