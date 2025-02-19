0de2cfa40c0b3fae6da78511f9c768792701af3d
Isaac Kelly <isaacwwkelly@gmail.com>
2025-02-19 15:09:50 -0600

    updated favicon & readme

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is a personal project created with NextJS and hosted on Vercel.
+This is a personal project created with NextJS/Tailwind CSS and hosted on Vercel. You can see the live app at www.isaacwwkelly.dev
--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -2,7 +2,9 @@ export default function Footer() {
   return (
     <div id="footer" className="bg-[var(--background)] flex justify-center">
       Icons by&nbsp;
-      <a href="https://icons8.com">Icons8</a>
+      <a href="https://icons8.com" target="_blank" rel="noopener noreferrer">
+        Icons8
+      </a>
     </div>
   );
 }
